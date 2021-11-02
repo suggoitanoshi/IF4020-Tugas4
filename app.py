aa4d520f99f6ea76af950ee48bfa999ea54ae5c9
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2021-11-02 11:11:30 +0700

    feat: add algos to routes

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import json
+import sys
 from flask import Flask,render_template,request
-from algos import rsa
+from algos import rsa, eg, paillier
 
 app = Flask(__name__)
 
@@ -13,7 +15,16 @@ def genkey_frontend():
 
 @app.post('/generatekey')
 def genkey():
-  return 'OK'
+  alg = request.form['alg']
+  if alg == 'RSA':
+    pubkey, privkey = rsa.genkey()
+    pubkey = hex(pubkey)
+    privkey = hex(privkey)
+  elif alg == 'EG':
+    pubkey, privkey = eg.encodekey(eg.genkey())
+  elif alg == 'Paillier':
+    pubkey, privkey = paillier.encodekey(paillier.genkey())
+  return {'pubkey': pubkey, 'privkey': privkey}
 
 @app.get('/encryptdecrypt')
 def ed_frontend():
@@ -28,9 +39,28 @@ def ed():
   except KeyError:
     msg = request.files['message'].read()
   try:
-    key = bytearray(request.form['key'], 'utf8')
+    key = request.form['key']
   except KeyError:
     key = request.files['key'].read()
+  if alg == 'RSA':
+    key = int(key, 16)
+    key = key.to_bytes((key+7)//8, byteorder=sys.byteorder, signed=False)
+    if op == 'enc':
+      return rsa.encrypt(msg, key)
+    elif op == 'dec':
+      return rsa.decrypt(msg, key)
+  elif alg == 'EG':
+    key = eg.decodekey(json.loads(key))
+    if op == 'enc':
+      return eg.encrypt(msg, key)
+    elif op == 'dec':
+      return eg.decrypt(msg, key)
+  elif alg == 'Paillier':
+    key = paillier.decodekey(json.loads(key))
+    if op == 'enc':
+      return paillier.encrypt(msg, key)
+    elif op == 'dec':
+      return paillier.decrypt(msg, key)
   return alg
 
 if __name__ == "__main__":
